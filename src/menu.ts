fccbc285d2d3ce92c70097f720088edba12bf0de
dalramjwi <dalramjwi@gmail.com>
2024-07-04 11:03:31 +0900

    a tag 함수 제작

--- src/menu.ts
+++ src/menu.ts
@@ -1,12 +1,16 @@
 const root = document.getElementById("root")!;
 console.log(root);
-const liTags = () => {
-  let result = `<li>목록</li>`;
+const aTags = () => {
+  let result = `<a href = "#">메뉴</a>`;
+  return result;
+};
+const liTags = (children: string) => {
+  let result = `<li>${children}</li>`;
   return result;
 };
 root.innerHTML = `
 <ul>
 <li>
-${liTags()}
+${liTags(aTags())}</li>
 </li>
 </ul>`;
