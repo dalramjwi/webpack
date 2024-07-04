49a585a019084b640509116fde20f9d1d92085e7
dalramjwi <dalramjwi@gmail.com>
2024-07-04 11:10:05 +0900

    make js

--- dist/menu.js
+++ dist/menu.js
@@ -2,15 +2,12 @@
 
 var root = document.getElementById("root");
 console.log(root);
-var address = function address(_address) {
-  return _address;
-};
-var aTags = function aTags(address) {
-  var result = "<a href = ".concat(address, ">\uBA54\uB274</a>");
+var aTags = function aTags(address, text) {
+  var result = "<a href = ".concat(address, ">").concat(text, "</a>");
   return result;
 };
 var liTags = function liTags(children) {
   var result = "<li>".concat(children, "</li>");
   return result;
 };
-root.innerHTML = "\n<ul>\n<li>\n".concat(liTags(aTags(address("#"))), "</li>\n</li>\n</ul>");
+root.innerHTML = "\n<ul>\n<li>\n".concat(liTags(aTags("#", "안녕")), "</li>\n</li>\n</ul>");
