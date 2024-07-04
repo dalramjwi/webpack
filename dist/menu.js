443e5c82818b0786fc1d436ca2db1c6ecf8063fb
dalramjwi <dalramjwi@gmail.com>
2024-07-04 10:59:49 +0900

    make li tag

--- dist/menu.js
+++ dist/menu.js
@@ -2,3 +2,4 @@
 
 var root = document.getElementById("root");
 console.log(root);
+root.innerHTML = "\n<ul>\n<li>\n  <a href=\"#\">Home</a>\n</li>\n</ul>";
--- src/menu.ts
+++ src/menu.ts
@@ -1,2 +1,12 @@
-const root = document.getElementById("root");
+const root = document.getElementById("root")!;
 console.log(root);
+const liTags = () => {
+  let result = ``;
+  return result;
+};
+root.innerHTML = `
+<ul>
+<li>
+  <a href="#">Home</a>
+</li>
+</ul>`;
